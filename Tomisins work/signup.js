8b09321825e126f2740f3696c6d5414cb653f762
Oluwaloni Richard <lonnyrichard77@gmail.com>
2019-05-24 10:35:59 +0100

    first commit

---
+++ Tomisins work/signup.js
@@ -0,0 +1,3 @@
+document.getElementById('formGroupExampleInput2').style.color = "#2940B8";
+document.getElementById('formGroupExampleInput').style.color = "#2940B8";
+document.getElementById('formGroupExampleInput23').style.color = "#2940B8";
---
+++ Wales Work/signin.js
@@ -0,0 +1,11 @@
+document.getElementById('formGroupExampleInput2').style.color = "#2940B8";
+document.getElementById('formGroupExampleInput').style.color = "#2940B8";
+var firstAjax = new XMLHttpRequest();
+xhr .onreadystatechange = function() {
+
+}
+var secondAjax = new XMLHttpRequest();
+xhr .onreadystatechange = function(){
+     
+} 
+    
